f9cc0f66f72726cea2c2443e75c2dcd8b9dd7a3d
donoh <201904014@o.cnu.ac.kr>
2023-06-24 14:54:06 +0900

    nodeJsInput changed

--- baekjoon/nodeJSInput.js
+++ baekjoon/nodeJSInput.js
@@ -1,13 +1,2 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-});
-
-const input = [];
-
-rl.on("line", (line) => {
-	input.push(line.split(" "));
-}).on("close", () => {
-	process.exit();
-});
+const input = `${require("fs").readFileSync(0)}`;
+console.log(input);
